999f325e49fad14198d0fd793a1d1d4830d4467d
lacornen <nlacorne@gmail.com>
2015-01-25 12:12:09 +0100

    Adding comments and modify function cacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file implements a cache mechanism for Matrix inversion.
+## This mechanism is implementated through a cacheMatrix list structure and 
+## use parent environment to cache inversion results. This structure is 
+## used by a custom Solve function cacheSolve that take a cacheMatrix
+## as an input an try to retrieve the cached inverse if available.
+## Otherwise, it calculate the invert and populate the cache.
+## 
+## 
+## This file contains definition of the functions makeCachematrix and cacheSolve
+## *****************************************************************************
 
-## Create CacheMatrix object with 4 function, that can store  matrix
-## and its invert.
-##
+## makeCacheMatrix :`
+## *****************
+## makeCacheMatrix creates CacheMatrix "object" with 4 function, that can store  
+## matrix and its invert.
 ## The functions set() and get(x) are used to set or get matrix data.
 ## The function setinv(x) is used to cached the inverse of the matrix
 ## The funciton getinv() is used to retrieve cached value.
 ##
-## In reality the object is a list of the 4 function that set set and
-## acaess the stored value in parent environment.
+## In reality the "object" is a list of the 4 function that set and
+## access the cached value and datas in parent environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-        #inv <- NULL
         set <- function(y) {
                 x   <<- y
                 inv <<- NULL
@@ -21,11 +29,22 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinv <- function(inverse) inv <<- inverse
         getinv <- function() inv
-        list(set = set, get = get, setinv= setinv, getinv = getinv)
+        cache_s <- list(set = set, get = get, setinv= setinv, getinv = getinv)
+        cache_s$set(x)
+        cache_s
 }
 
 
-## Write a short comment describing this function
+## cacheSolve :`
+## ************
+## cacheSolve is a custom Solve function. It takes a cacheMatrix list as input.
+## It first test if the cache invert is setted.
+## If there is an invert cached , it retrieves the invert from the environment 
+## using getinv() and skip the computation.
+## Otherwise is retrieve the Matrix datas using get(), 
+## run invert computation using function solve on them
+## and finally cache the result using setinv().
+## The inverted matrix is returned in both case.
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
